6dad4c9ba605e50389a55d776742e48d8c724486
BossWasHere <22352723+BossWasHere@users.noreply.github.com>
2021-10-29 11:23:38 +0100

    Project Init

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    base: '/',
+    resolve: {
+        alias: {
+            '@/': resolve(__dirname, 'src') + '/',
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '',
+            },
+        },
+    },
+});
